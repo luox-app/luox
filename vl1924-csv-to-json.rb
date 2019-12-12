05b42f8545bd1b3182ce93157f2f7e1363da6b86
Ben Griffiths <bengriffiths@gmail.com>
2019-12-12 16:29:33 +0000

    Add vl1924 data and conversion script
    To convert it from CSV to JSON.
    
    Source vl1924 data was provided by Manuel.

---
+++ vl1924-csv-to-json.rb
@@ -0,0 +1,15 @@
+require 'csv'
+require 'json'
+
+data = {}
+
+File.open('vl1924.csv') do |file|
+  file.each_line do |line|
+    row = CSV.parse_line(line.strip).map(&:strip)
+    data[row.first.to_i] = row.last.to_f
+  end
+end
+
+File.open('vl1924.json', 'w') do |file|
+  file.puts(data.to_json)
+end
