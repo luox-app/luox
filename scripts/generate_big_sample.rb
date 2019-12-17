e1989fb5b0b3008a670b875fa31026512c42f2a2
Ben Griffiths <bengriffiths@gmail.com>
2019-12-17 10:01:28 +0000

    Add scripts to generate large (300 samples) csv file for testing

---
+++ scripts/generate_big_sample.rb
@@ -0,0 +1,26 @@
+require 'csv'
+
+data = []
+
+NUM_SAMPLES = 300
+
+def randomise(samples)
+  [rand(0.0001..0.0002)] * NUM_SAMPLES
+end
+
+File.open('data/example-spectrum.csv') do |file|
+  file.each_line do |line|
+    wavelength, *samples = CSV.parse_line(line.scrub)
+    if wavelength =~ /^\d+/
+      data << [wavelength] + randomise(samples)
+    end
+  end
+end
+
+File.open('src/big-sample.csv', 'w') do |file|
+  sampleHeaders = (1..NUM_SAMPLES).map { |s| "S#{s}" }
+  file.puts((["wavelength"] + sampleHeaders).to_csv)
+  data.each do |d|
+    file.puts(d.to_csv)
+  end
+end
