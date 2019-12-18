3922bc47e2e464d97cc2778258750a894366341e
Chris Roos <chris@seagul.co.uk>
2019-12-18 15:55:10 +0000

    Move UI functions

--- src/index.js
+++ src/index.js
@@ -1,41 +1,5 @@
-import {downloadCSVButton} from './csvExport.js'
-import {mapSamples, calculateLuminance, calculateIrradiance, interpolateData} from './rows.js'
+import {createTables} from './ui.js'
 import Papa from 'papaparse'
-
-const asExponential = (number) => number.toExponential(2)
-const asDecimal = (number) => number.toFixed(2)
-
-const appendCells = (table, cellType, cells) => {
-  const row = document.createElement("tr")
-  const domCells = cells.map((cellText) =>  {
-    const cell = document.createElement(cellType)
-    const text = document.createTextNode(cellText)
-    cell.appendChild(text)
-    return cell
-  })
-  for (const cell of domCells) {
-    row.appendChild(cell)
-  }
-  table.appendChild(row)
-  return domCells
-}
-
-const createSampleTableHeader = (table, sampleCount) => {
-  const titles = ["Wavelength [nm]", "Spectral irradiance [W/(m² nm)]"]
-  const cells = appendCells(table, "th", titles)
-  cells[1].setAttribute("colspan", sampleCount)
-}
-
-const createTableHeader = (table, sampleCount) => {
-  const sampleTitles = new Array(sampleCount).fill("").map((_, index) => "S"+index)
-  const titles = ["Condition", ...sampleTitles]
-  appendCells(table, "th", titles)
-}
-
-const createTableRow = (table, wavelength, samples, formatter) => {
-  const formattedSamples = samples.map(formatter)
-  appendCells(table, "td", [wavelength, ...formattedSamples])
-}
 
 const parseCSV = async (file) => {
   const fileContents = await file.text()
@@ -68,37 +32,6 @@ const handleFileSelect = () => {
   fileButton.disabled = fileList.length === 0
 }
 
-const createTables = (rawRows, sampleCount, spectrumTable, calculationTable, unitConversion, footerButtons) => {
-  const rows = mapSamples(rawRows, unitConversion)
-  const interpolatedRows = interpolateData(rows, sampleCount)
-
-  createTableHeader(calculationTable, sampleCount)
-
-  const luminanceTotals = calculateLuminance(interpolatedRows, sampleCount)
-  const sConeTotals = calculateIrradiance(interpolatedRows, sampleCount, 'sCone')
-  const mConeTotals = calculateIrradiance(interpolatedRows, sampleCount, 'mCone')
-  const lConeTotals = calculateIrradiance(interpolatedRows, sampleCount, 'lCone')
-  const rodTotals = calculateIrradiance(interpolatedRows, sampleCount, 'rod')
-  const melTotals = calculateIrradiance(interpolatedRows, sampleCount, 'mel')
-  createTableRow(calculationTable, "Illuminance [lux]", luminanceTotals, asDecimal)
-  createTableRow(calculationTable, "S-cone-opic irradiance (mW/m²)", sConeTotals, asDecimal)
-  createTableRow(calculationTable, "M-cone-opic irradiance (mW/m²)", mConeTotals, asDecimal)
-  createTableRow(calculationTable, "L-cone-opic irradiance (mW/m²)", lConeTotals, asDecimal)
-  createTableRow(calculationTable, "Rhodopic irradiance (mW/m²)", rodTotals, asDecimal)
-  createTableRow(calculationTable, "Melanopic irradiance (mW/m²)", melTotals, asDecimal)
-
-  createSampleTableHeader(spectrumTable, sampleCount)
-  for (const row of rows) {
-    const [wavelength, ...samples] = row
-    createTableRow(spectrumTable, wavelength, samples, asExponential)
-  }
-
-  const calcCSVButton = downloadCSVButton(calculationTable, "btn btn-primary", "download-calc", "Download calculation CSV")
-  footerButtons.appendChild(calcCSVButton);
-  const spectrumCSVButton = downloadCSVButton(spectrumTable, "btn btn-primary", "download-spectrum", "Download spectrum CSV")
-  footerButtons.appendChild(spectrumCSVButton);
-}
-
 const createErrorTable = (errors) => {
   const header = document.createElement('p')
   const textNode = document.createTextNode('We had some problems understanding that file:')
@@ -126,7 +59,6 @@ const createErrorTable = (errors) => {
   }
   fileUploadedSection.appendChild(table)
 }
-
 
 const handleSubmit = async (event) => {
   event.preventDefault();
