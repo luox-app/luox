4efdb7922d5b8a0bad33f59705bdb93c2bb5a080
Chris Lowis <chris.lowis@gmail.com>
2020-02-24 22:25:14 +0000

    Always display two digits in exponent of scientific notation
    https://trello.com/c/ObCWvm9Q/30-update-scientific-notation-format
    
    We prefer to always display two digits in the exponent when using
    scientific notation, e.g. 1.36e+02 instead of 1.36e+2
    
    Eslint wants me to name the capture group using e.g ?<name>[1]. This
    is an ECMAScript 2018 feature, and I'm not sure it improves things in
    this case, so I've disabled it for this function.
    
    [1] https://eslint.org/docs/rules/prefer-named-capture-group

--- src/helpers.js
+++ src/helpers.js
@@ -1,2 +1,8 @@
-export const asExponential = (number) => number.toExponential(2)
+/* eslint-disable prefer-named-capture-group */
+export const asExponential = (number) => {
+  const parts = number.toExponential(2).split(/(e\+|-)/)
+  return parts[0] + parts[1] + parts[2].padStart(2, '0')
+}
+/* eslint-enable prefer-named-capture-group */
+
 export const asDecimal = (number) => number.toFixed(2)
--- test/helper.test.js
+++ test/helper.test.js
@@ -3,14 +3,14 @@ var assert = require('assert');
 
 describe('asExponential', function() {
   it('should return a number larger than 1 in scientific notation', function() {
-    assert.equal('1.36e+2', asExponential(136))
+    assert.equal('1.36e+02', asExponential(136))
   });
 
   it('should return a number smaller than 1 in scientific notation', function() {
-    assert.equal('1.36e-2', asExponential(0.0136))
+    assert.equal('1.36e-02', asExponential(0.0136))
   });
 
-  it('should have two digits in the exponent when appropriate', function() {
+  it('should not have a leading zero in the exponent for larger numbers', function() {
     assert.equal('1.36e+10', asExponential(13600000000))
   });
 });
