95bc721dd3f9f532d7430f9297b83b1df9adfc24
Chris Roos <chris@seagul.co.uk>
2020-04-13 10:45:46 +0100

    Move chart functions to new file
    To group related functionality.

---
+++ src/chart.js
@@ -0,0 +1,101 @@
+import {mapSamples} from './rows.js'
+import {sampleTitles} from './helpers.js'
+import Chart from 'chart.js'
+
+const generateHues = (sampleCount) => {
+  const hues = []
+  const hueInterval = 360 / sampleCount
+  for (let i = 0; i < sampleCount; i += 1) {
+    hues.push(i * hueInterval)
+  }
+  return hues
+}
+
+/* eslint-disable max-lines-per-function */
+export const createChart = (chartCanvas, rows, sampleCount) => {
+  const datasets = []
+  const hues = generateHues(sampleCount)
+  const labels = sampleTitles(sampleCount)
+
+  for (let sampleIdx = 0; sampleIdx < sampleCount; sampleIdx += 1) {
+    const lineColor = 'hsl(' + hues[sampleIdx] + ',100%,50%)'
+    datasets[sampleIdx] = {
+      'backgroundColor': lineColor,
+      'borderColor': lineColor,
+      'data': rows.map((row) => row[sampleIdx + 1]),
+      'fill': false,
+      'label': labels[sampleIdx],
+      'pointRadius': 1
+    }
+  }
+  const waveLengths = rows.map((row) => row[0]);
+
+  return new Chart(chartCanvas, { // eslint-disable-line no-new
+    'data': {
+      datasets,
+      'labels': waveLengths
+    },
+    'options': {
+      'scales': {
+        'xAxes': [
+          {
+            'scaleLabel': {
+              'display': true,
+              'labelString': 'Wavelength [nm]'
+            },
+            'ticks': {
+              'maxTicksLimit': 20
+            }
+          }
+        ],
+        'yAxes': [
+          {
+            'scaleLabel': {
+              'display': true,
+              'labelString': 'Spectral irradiance [W/(m² nm)]'
+            }
+          }
+        ]
+      }
+    },
+    'type': 'line'
+  });
+}
+/* eslint-enable max-lines-per-function */
+
+export const addDataSourcesToChart = (chart, rows, sampleCount) => {
+  const maxValues = []
+  for (let sampleIdx = 0; sampleIdx < sampleCount; sampleIdx += 1) {
+    const spectrum = rows.map((row) => row[sampleIdx + 1])
+    maxValues[sampleIdx] = Math.max(...spectrum)
+  }
+  const normalisedRows = mapSamples(rows, (wavelength, sample, sampleIndex) => {
+    return sample / maxValues[sampleIndex]
+  })
+
+  const log10Rows = mapSamples(rows, (wavelength, sample) => {
+    return Math.log10(sample)
+  })
+
+  $('#chart-data-source input[name="chart-data"]').click((event) => {
+    let data = []
+    let yAxisLabel = ''
+    if (event.target.value === 'raw') {
+      yAxisLabel = 'Spectral irradiance [W/(m² nm)]'
+      data = rows
+    } else if (event.target.value === 'normalised') {
+      yAxisLabel = 'Normalised spectral irradiance (relative to max.)'
+      data = normalisedRows
+    } else if (event.target.value === 'log10') {
+      yAxisLabel = 'Log₁₀ spectral irradiance [log₁₀ W/(m² nm)]'
+      data = log10Rows
+    }
+    chart.options.scales.yAxes[0].scaleLabel.labelString = yAxisLabel
+    chart.data.datasets.forEach((dataset, index) => {
+      dataset.data = data.map((row) => row[index + 1])
+    })
+    chart.update()
+  })
+  $('#chart-data-source input#chart-data-raw').prop('checked', true)
+  $('#chart-data-source').show()
+}
--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,8 @@
 import {mapSamples, interpolateData} from './rows.js'
 import {createCalculationTable} from './calculationTable.js'
 import {createSpectraTable} from './spectraTable.js'
-import {sampleTitles} from './helpers.js'
 import {downloadCSVButton} from './csvExport.js'
-import Chart from 'chart.js'
+import {addDataSourcesToChart, createChart} from './chart.js'
 
 const conversionFunction = (areaScale, powerScale) => {
   return (wavelength, sample) =>  sample / powerScale * areaScale
@@ -45,109 +44,15 @@ const createActionsForTables = (calculationTable, spectrumTable) => {
   document.getElementById('spectra-table-actions').appendChild(spectraToggle)
 }
 
-const generateHues = (sampleCount) => {
-  const hues = []
-  const hueInterval = 360 / sampleCount
-  for (let i = 0; i < sampleCount; i += 1) {
-    hues.push(i * hueInterval)
-  }
-  return hues
-}
-
-/* eslint-disable max-lines-per-function */
-const createChart = (chartCanvas, rows, sampleCount) => {
-  const datasets = []
-  const hues = generateHues(sampleCount)
-  const labels = sampleTitles(sampleCount)
-
-  for (let sampleIdx = 0; sampleIdx < sampleCount; sampleIdx += 1) {
-    const lineColor = 'hsl(' + hues[sampleIdx] + ',100%,50%)'
-    datasets[sampleIdx] = {
-      'backgroundColor': lineColor,
-      'borderColor': lineColor,
-      'data': rows.map((row) => row[sampleIdx + 1]),
-      'fill': false,
-      'label': labels[sampleIdx],
-      'pointRadius': 1
-    }
-  }
-  const waveLengths = rows.map((row) => row[0]);
-
-  return new Chart(chartCanvas, { // eslint-disable-line no-new
-    'data': {
-      datasets,
-      'labels': waveLengths
-    },
-    'options': {
-      'scales': {
-        'xAxes': [
-          {
-            'scaleLabel': {
-              'display': true,
-              'labelString': 'Wavelength [nm]'
-            },
-            'ticks': {
-              'maxTicksLimit': 20
-            }
-          }
-        ],
-        'yAxes': [
-          {
-            'scaleLabel': {
-              'display': true,
-              'labelString': 'Spectral irradiance [W/(m² nm)]'
-            }
-          }
-        ]
-      }
-    },
-    'type': 'line'
-  });
-}
-/* eslint-enable max-lines-per-function */
-
-/* eslint-disable max-lines-per-function */
 export const createTables = (rawRows, sampleCount, spectrumTable, calculationTable, areaScale, powerScale, footerButtons, chartCanvas, simplifiedReport) => {
   const unitConversion = conversionFunction(areaScale, powerScale)
   const rows = mapSamples(rawRows, unitConversion)
   const interpolatedRows = interpolateData(rows, sampleCount)
 
-  const maxValues = []
-  for (let sampleIdx = 0; sampleIdx < sampleCount; sampleIdx += 1) {
-    const spectrum = rows.map((row) => row[sampleIdx + 1])
-    maxValues[sampleIdx] = Math.max(...spectrum)
-  }
-  const normalisedRows = mapSamples(rows, (wavelength, sample, sampleIndex) => {
-    return sample / maxValues[sampleIndex]
-  })
-  const log10Rows = mapSamples(rows, (wavelength, sample) => {
-    return Math.log10(sample)
-  })
-
   createCalculationTable(calculationTable, interpolatedRows, sampleCount, simplifiedReport)
 
   const chart = createChart(chartCanvas, rows, sampleCount)
-  $('#chart-data-source input[name="chart-data"]').click((event) => {
-    let data = []
-    let yAxisLabel = ''
-    if (event.target.value === 'raw') {
-      yAxisLabel = 'Spectral irradiance [W/(m² nm)]'
-      data = rows
-    } else if (event.target.value === 'normalised') {
-      yAxisLabel = 'Normalised spectral irradiance (relative to max.)'
-      data = normalisedRows
-    } else if (event.target.value === 'log10') {
-      yAxisLabel = 'Log₁₀ spectral irradiance [log₁₀ W/(m² nm)]'
-      data = log10Rows
-    }
-    chart.options.scales.yAxes[0].scaleLabel.labelString = yAxisLabel
-    chart.data.datasets.forEach((dataset, index) => {
-      dataset.data = data.map((row) => row[index + 1])
-    })
-    chart.update()
-  })
-  $('#chart-data-source input#chart-data-raw').prop('checked', true)
-  $('#chart-data-source').show()
+  addDataSourcesToChart(chart, rows, sampleCount)
 
   createSpectraTable(spectrumTable, rows, sampleCount)
 
@@ -158,7 +63,6 @@ export const createTables = (rawRows, sampleCount, spectrumTable, calculationTab
     createDownloadButtonsInFooter(footerButtons, calculationTable, spectrumTable)
   }
 }
-/* eslint-enable max-lines-per-function */
 
 export const createErrorTable = (errors, errorsTable) => {
   for (const error of errors) {
