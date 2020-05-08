e14cbb61a8e36ca60c63919f3569150d3b6da552
Chris Roos <chris@seagul.co.uk>
2020-05-08 16:33:58 +0100

    Extract renderXXX methods
    To grouped related functionality within the createResults function.

--- src/javascript/results-ui.js
+++ src/javascript/results-ui.js
@@ -59,11 +59,7 @@ const createNotationToggle = (table, defaultNotation, decimalPrecision) => {
   return notationToggle;
 }
 
-export const createResults = (rawRows, sampleCount, areaScale, powerScale, simplifiedReport) => {
-  const unitConversion = conversionFunction(areaScale, powerScale)
-  const rows = mapSamples(rawRows, unitConversion)
-  const interpolatedRows = interpolateData(rows, sampleCount)
-
+const renderCalculations = (interpolatedRows, sampleCount, simplifiedReport) => {
   const calculations = document.getElementById('calculations')
   const calculationTable = document.getElementById('calculation-table')
   createCalculationTable(calculationTable, interpolatedRows, sampleCount, simplifiedReport)
@@ -74,7 +70,9 @@ export const createResults = (rawRows, sampleCount, areaScale, powerScale, simpl
     createNotationToggle(calculationTable, 'decimal', 2)
   )
   calculations.getElementsByClassName('heading-and-actions')[0].append(calculationActions)
+}
 
+const renderChart = (rows, sampleCount) => {
   const chart = document.getElementById('chart')
   const chartCanvas = document.getElementById('chart-canvas')
   createChart(chartCanvas, rows, sampleCount)
@@ -85,7 +83,9 @@ export const createResults = (rawRows, sampleCount, areaScale, powerScale, simpl
     createToggleButton(chartAndControls, 'false', 'Toggle chart')
   )
   chart.getElementsByClassName('heading-and-actions')[0].append(chartActions)
+}
 
+const renderSpectra = (rows, sampleCount) => {
   const spectra = document.getElementById('spectra')
   const spectrumTable = document.getElementById('spectrum-table')
   createSpectraTable(spectrumTable, rows, sampleCount)
@@ -97,4 +97,14 @@ export const createResults = (rawRows, sampleCount, areaScale, powerScale, simpl
     createNotationToggle(spectrumTable, 'scientific', 10)
   )
   spectra.getElementsByClassName('heading-and-actions')[0].append(spectraActions)
+}
+
+export const createResults = (rawRows, sampleCount, areaScale, powerScale, simplifiedReport) => {
+  const unitConversion = conversionFunction(areaScale, powerScale)
+  const rows = mapSamples(rawRows, unitConversion)
+  const interpolatedRows = interpolateData(rows, sampleCount)
+
+  renderCalculations(interpolatedRows, sampleCount, simplifiedReport)
+  renderChart(rows, sampleCount)
+  renderSpectra(rows, sampleCount)
 }
