luox-app/luox

commit 86a3d6a25229c75ed8d9ef532cdee43dfa0dc154

Group similar functionality in renderXXX methods

src/javascript/results-ui.js
@@ -55,52 +55,59 @@ const createNotationToggle = (table, defaultNotation, decimalPrecision) => {
 }
 
 const renderCalculations = (container, interpolatedRows, sampleCount, simplifiedReport) => {
-  const table = document.createElement('table')
-  table.className = 'table table-sm mt-3 result-table'
-  createCalculationTable(table, interpolatedRows, sampleCount, simplifiedReport)
   const heading = document.createElement('h3')
-  heading.append('Illuminance and α-opic irradiances')
   const actions = document.createElement('div')
+  const table = document.createElement('table')
+
+  heading.append('Illuminance and α-opic irradiances')
   actions.append(
     downloadCSVButton(table, "download-calc"),
     createToggleButton(table, 'true', 'Toggle table'),
     createNotationToggle(table, 'decimal', 2)
   )
   container.getElementsByClassName('heading-and-actions')[0].append(heading, actions)
+
+  table.className = 'table table-sm mt-3 result-table'
+  createCalculationTable(table, interpolatedRows, sampleCount, simplifiedReport)
   container.append(table)
 }
 
 const renderChart = (container, rows, sampleCount) => {
-  const canvas = document.createElement('canvas')
-  canvas.setAttribute('width', '400')
-  canvas.setAttribute('height', '200')
-  const chartAndControls = document.getElementById('spectra-chart-and-controls')
-  chartAndControls.append(canvas)
-  chartAndControls.style.display = 'none'
-  createChart(canvas, rows, sampleCount)
   const heading = document.createElement('h3')
-  heading.append('Spectra chart')
   const actions = document.createElement('div')
+  const chartAndControls = document.getElementById('spectra-chart-and-controls')
+  const canvas = document.createElement('canvas')
+
+  heading.append('Spectra chart')
   actions.append(
     createToggleButton(chartAndControls, 'false', 'Toggle chart')
   )
   container.getElementsByClassName('heading-and-actions')[0].append(heading, actions)
+
+  chartAndControls.append(canvas)
+  chartAndControls.style.display = 'none'
+
+  canvas.setAttribute('width', '400')
+  canvas.setAttribute('height', '200')
+  createChart(canvas, rows, sampleCount)
 }
 
 const renderSpectra = (container, rows, sampleCount) => {
-  const table = document.createElement('table')
-  table.className = 'table table-sm mt-3 result-table'
-  createSpectraTable(table, rows, sampleCount)
-  table.style.display = 'none'
   const heading = document.createElement('h3')
-  heading.append('Spectra')
   const actions = document.createElement('div')
+  const table = document.createElement('table')
+
+  heading.append('Spectra')
   actions.append(
     downloadCSVButton(table, "download-spectrum"),
     createToggleButton(table, 'false', 'Toggle table'),
     createNotationToggle(table, 'scientific', 10)
   )
   container.getElementsByClassName('heading-and-actions')[0].append(heading, actions)
+
+  table.className = 'table table-sm mt-3 result-table'
+  table.style.display = 'none'
+  createSpectraTable(table, rows, sampleCount)
   container.append(table)
 }
 

commit a747a2a0fcbd42dae24210a4917b1434c95f1666

Fix broken "Download CSV" links
I broke this functionality in 86a3d6a25229c75ed8d9ef532cdee43dfa0dc154.

We need to have populated the table before calling the
`downloadCSVButton` function. Otherwise, we create the CSV from the
empty table!

src/javascript/results-ui.js
@@ -64,6 +64,8 @@ const renderCalculations = (radianceOrIrradiance, interpolatedRows, sampleCount,
   headingAndActions.className = 'mt-3 d-flex justify-content-between'
   table.className = 'table table-sm mt-3 result-table'
 
+  createCalculationTable(table, radianceOrIrradiance, interpolatedRows, sampleCount, simplifiedReport)
+
   heading.append(`Illuminance and α-opic ${radianceOrIrradiance}s`)
   actions.append(
     downloadCSVButton(table, "download-calc"),
@@ -71,8 +73,6 @@ const renderCalculations = (radianceOrIrradiance, interpolatedRows, sampleCount,
     createNotationToggle(table, 'decimal', 2)
   )
   headingAndActions.append(heading, actions)
-
-  createCalculationTable(table, radianceOrIrradiance, interpolatedRows, sampleCount, simplifiedReport)
   container.append(headingAndActions, table)
   return container
 }
@@ -114,6 +114,8 @@ const renderSpectra = (radianceOrIrradiance, rows, sampleCount) => {
   table.className = 'table table-sm mt-3 result-table'
   table.style.display = 'none'
 
+  createSpectraTable(table, radianceOrIrradiance, rows, sampleCount)
+
   heading.append('Spectra')
   actions.append(
     downloadCSVButton(table, "download-spectrum"),
@@ -121,8 +123,6 @@ const renderSpectra = (radianceOrIrradiance, rows, sampleCount) => {
     createNotationToggle(table, 'scientific', 10)
   )
   headingAndActions.append(heading, actions)
-
-  createSpectraTable(table, radianceOrIrradiance, rows, sampleCount)
   container.append(headingAndActions, table)
   return container
 }