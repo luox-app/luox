4c4c58707d815fbd8e48e6fcccdbb9720d9a5a24
Chris Lowis <chris.lowis@gmail.com>
2020-06-12 16:24:26 +0100

    Implementation of blackbodyReferenceSpectra
    This function calculates the spectra for a "Plankian radiator" at a
    given colour temperature, required for step 2 of the CRI method.
    
    The values we calculated using blackBodyReferenceSpectra disagreed by
    a factor of 100 with the values in the table in step 2 of section A7.2
    in Hunt and Pointer[1]. I asked Manuel about this and he said:
    
        At the end of paragraph 1, it says “The value of c1, however, is
        not relevant in colorimetry where only the relative spectral
        distribution of Planck’s radiation is used.” Indeed, for the CRI
        calculations, only colour, not absolute luminance or illuminance
        will be used, so it is scale independent.
    
        The normalisation constant is simply to normalise it, so for now,
        I’d say leave it in as is.
    
    He's happy that the calculation is correct, so we've chosen to use
    three arbitrarily chosen values from that table (divided by 100) as
    our test data.
    
    [1] Measuring Colour, Fourth Edition. R.W.G. Hunt and M.R. Pointer.

--- src/javascript/colourRenderingIndex.js
+++ src/javascript/colourRenderingIndex.js
@@ -14,3 +14,25 @@ export const correlatedColourTemperature = (x, y) => {
   return cct
 }
 
+// Cite: CIE 015:2018 Annex E
+const blackBodyspectralRadiance = (lambda, t) => {
+  const pi = Math.PI
+  // Plank constant
+  const h = 6.62607015e-34
+  // Boltzmann constant
+  const k = 1.380649e-23
+  // Speed of light in a vacuum
+  const c = 299792458
+  // Refractive index for dry air
+  const n = 1
+
+  const c1 = 2 * pi * h * (c**2)
+  const c2 = h * c / k
+
+  return ((c1 * (n**-2) * (lambda ** -5)) / pi) * (1 / (Math.exp(c2 / (n * lambda * t)) - 1))
+}
+
+// Cite: CIE 015:2018 Annex E
+export const blackBodyReferenceSpectra = (lambda, T) => {
+  return blackBodyspectralRadiance(lambda, T) / blackBodyspectralRadiance(560e-9, T)
+}
--- test/colourRenderingIndex.test.js
+++ test/colourRenderingIndex.test.js
@@ -1,4 +1,4 @@
-var { cie1960UCS, correlatedColourTemperature } = require('./../src/javascript/colourRenderingIndex.js')
+var { cie1960UCS, correlatedColourTemperature, blackBodyReferenceSpectra } = require('./../src/javascript/colourRenderingIndex.js')
 var assert = require('assert')
 
 describe('cie1960UCS', function() {
@@ -17,5 +17,15 @@ describe('correlatedColourTemperature', function() {
   it('calculates CCT using the McCarmy approximation', function() {
     const cct = correlatedColourTemperature(0.3721, 0.3751)
     assert.equal(cct.toFixed(1), 4228.8)
+  });
+});
+
+describe('blackBodyReferenceSpectra', function() {
+  it('calculates the blackbody reference spectra at temperature T', function() {
+    const temperature = 4224.4;
+    // Test data from Hunt and Pointer (2011), Appendix 7
+    assert.equal(1.112, blackBodyReferenceSpectra(700e-9, temperature).toFixed(3))
+    assert.equal(0.848, blackBodyReferenceSpectra(500e-9, temperature).toFixed(3))
+    assert.equal(0.674, blackBodyReferenceSpectra(450e-9, temperature).toFixed(3))
   });
 });
