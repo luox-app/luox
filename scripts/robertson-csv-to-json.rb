c66c843c3afb7636f004973472ed9e910b581699
Chris Lowis <chris.lowis@gmail.com>
2020-07-02 15:07:25 +0100

    Add Robertson isotemperature lines data
    This is a direct copy of the table of data in the python
    colour-science package[1] and includes the Bruce Lindbloom correction
    for the 325 Megakelvin temperature.
    
    [1]
    https://github.com/colour-science/colour/blob/fe6312011f33f504ee66de285f2864d7249fdae6/colour/temperature/robertson1968.py#L57

---
+++ scripts/robertson-csv-to-json.rb
@@ -0,0 +1,22 @@
+require 'csv'
+require 'json'
+
+data = {}
+idx = 1
+
+File.open('data/robertson.csv') do |file|
+  file.each_line do |line|
+    row = CSV.parse_line(line.strip).map(&:strip)
+    data[idx] = {
+      'r' => row[0].to_f,
+      'u' => row[1].to_f,
+      'v' => row[2].to_f,
+      't' => row[3].to_f,
+    }
+    idx += 1
+  end
+end
+
+File.open('dist/robertson.json', 'w') do |file|
+  file.puts(data.to_json)
+end
