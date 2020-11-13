ad3c7307952248e89869a231691a1f067b858f1b
Chris Lowis <chris.lowis@gmail.com>
2020-11-13 10:55:21 +0000

    Improve styling of chart and add explanatory text

--- src/javascript/components/Chart.jsx
+++ src/javascript/components/Chart.jsx
@@ -52,72 +52,91 @@ const Chart = ({
 
   return (
     <section>
-      <form>
-        <div className="form-check-inline">
-          <label htmlFor="raw-y-axis" className="form-check-label">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="chart-data"
-              value="raw"
-              checked={yAxisScaling === "raw"}
-              onChange={handleYAxisScaling}
-              id="raw-y-axis"
-            />
-            Raw data
-          </label>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <div className="row">
+              <div>
+                <h5>Y-axis scale</h5>
+                <form>
+                  <div className="form-check">
+                    <label htmlFor="raw-y-axis" className="form-check-label">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="chart-data"
+                        value="raw"
+                        checked={yAxisScaling === "raw"}
+                        onChange={handleYAxisScaling}
+                        id="raw-y-axis"
+                      />
+                      Raw data
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <label
+                      htmlFor="normalised-y-axis"
+                      className="form-check-label"
+                    >
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="chart-data"
+                        value="normalised"
+                        checked={yAxisScaling === "normalised"}
+                        onChange={handleYAxisScaling}
+                        id="normalised-y-axis"
+                      />
+                      Normalised data
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <label htmlFor="log10-y-axis" className="form-check-label">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="chart-data"
+                        value="log10"
+                        checked={yAxisScaling === "log10"}
+                        onChange={handleYAxisScaling}
+                        id="log10-y-axis"
+                      />
+                      Log10
+                    </label>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="row mt-3">
+              <h5>Reference spectrum</h5>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="referenceSpectraSelect">
+                    Reference spectra
+                    <select
+                      className="form-control"
+                      id="referenceSpectraSelect"
+                      onChange={handleReferenceSpectraSelect}
+                    >
+                      <option value="none" key="none">
+                        Select a reference spectrum
+                      </option>
+                      {referenceSpectraNames().map((name) => (
+                        <option value={name} key={name}>
+                          {name}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="col-8">
+            <canvas width="400" height="200" ref={chartRef} />
+          </div>
         </div>
-        <div className="form-check-inline">
-          <label htmlFor="normalised-y-axis" className="form-check-label">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="chart-data"
-              value="normalised"
-              checked={yAxisScaling === "normalised"}
-              onChange={handleYAxisScaling}
-              id="normalised-y-axis"
-            />
-            Normalised data
-          </label>
-        </div>
-        <div className="form-check-inline">
-          <label htmlFor="log10-y-axis" className="form-check-label">
-            <input
-              className="form-check-input"
-              type="radio"
-              name="chart-data"
-              value="log10"
-              checked={yAxisScaling === "log10"}
-              onChange={handleYAxisScaling}
-              id="log10-y-axis"
-            />
-            Log10
-          </label>
-        </div>
-      </form>
-      <form>
-        <div className="form-group">
-          <label htmlFor="referenceSpectraSelect">
-            Reference spectra
-            <select
-              className="form-control"
-              id="referenceSpectraSelect"
-              onChange={handleReferenceSpectraSelect}
-            >
-              <option value="none" key="none">
-                Select a reference spectrum
-              </option>
-              {referenceSpectraNames().map((name) => (
-                <option value={name} key={name}>
-                  {name}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>
-      </form>
-      <canvas width="400" height="200" ref={chartRef} />
+      </div>
     </section>
   );
 };
