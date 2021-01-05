6312b661fc95993d78dcc7ee8061c0f0352db16f
James Mead <james@floehopper.org>
2021-01-05 13:03:01 +0000

    Avoid warnings related to source maps in Chrome Developer Tools
    I was seeing a bunch of warnings like the following:
    
        DevTools failed to load SourceMap: Could not load content for webpack:///node_modules/sockjs-client/dist/sockjs.js.map: HTTP error: status code 404, net::ERR_UNKNOWN_URL_SCHEME
    
    Installing and configuring the source-map-loader as per these
    instructions [1] seems to have fixed the problem. Note that I had to
    specify a slightly old version of this package to avoid a build
    incompatibility (with loader-utils, I think).
    
    [1]: https://webpack.js.org/loaders/source-map-loader/

--- webpack.common.js
+++ webpack.common.js
@@ -28,6 +28,11 @@ module.exports = {
         },
       },
       {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
