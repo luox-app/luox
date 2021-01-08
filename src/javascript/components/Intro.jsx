8e2bd181fe3257701b1ade8d92ab3e3f9ea86881
James Mead <james@floehopper.org>
2021-01-08 17:12:16 +0000

    WIP: Add example tooltip using reactstrap package
    As far as I can see this doesn't make the JS payload for the app
    significantly bigger.
    
    I'm not completely sold on using reactstrap. We might be better using
    react-bootstrap or trying to roll something ourselves.

--- src/javascript/components/Intro.jsx
+++ src/javascript/components/Intro.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Tooltip } from "reactstrap";
 import { Link } from "react-router-dom";
 
 const Intro = () => {
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  const toggle = () => setTooltipOpen(!tooltipOpen);
+
   return (
     <div className="row">
       <div className="col-lg-8">
@@ -21,10 +26,31 @@ const Intro = () => {
 
         <ul>
           <li>
-            Measure and report the spectral power distribution of the acute
-            stimulus from the observer’s point of view at a known and specified
-            angle and distance from the source
+            Measure and report the spectral power distribution of the{" "}
+            <span
+              style={{ borderBottom: "1px", borderBottomStyle: "dashed" }}
+              id="acute-stimulus-help"
+            >
+              acute stimulus
+            </span>{" "}
+            from the observer’s point of view at a known and specified angle and
+            distance from the source
           </li>
+          <Tooltip
+            placement="auto"
+            autohide={false}
+            isOpen={tooltipOpen}
+            target="acute-stimulus-help"
+            toggle={toggle}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            ut nunc nulla. In ac vestibulum quam. Nulla mollis nulla ligula,
+            eget blandit augue sodales vitae.{" "}
+            <a href="https://en.wikipedia.org/wiki/Stimulus_(physiology)">
+              Nunc maximus magna
+            </a>{" "}
+            sed lorem aucibus, vitae tempus enim varius.
+          </Tooltip>
           <li>
             Measure and report the spectral power distribution of the background
             light environment from the observer’s point of view at a known and
