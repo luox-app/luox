7303623e48f1a6d33fb3ab15948882419f26464d
James Mead <james@floehopper.org>
2021-02-02 22:10:19 +0000

    Remove sprague function and associated test & docs
    This become redundant in this commit [1] when we stopped interpolating
    input data.
    
    /cc @spitschan
    
    [1]: 3b327274ff81ee0243456b2cea938695cab1aa96

--- src/javascript/rows.js
+++ src/javascript/rows.js
@@ -1,5 +1,3 @@
-import sprague from "./sprague";
-
 export const mapSamples = (rows, func) => {
   return rows.map((row) => {
     const [wavelength, ...samples] = row;
@@ -32,36 +30,6 @@ export const integrateWithWeights = (rows, sampleCount, data, key) => {
     sampleCount,
     (w, runningTotal, sample) => runningTotal + sample
   );
-};
-
-export const interpolateData = (rows, sampleCount) => {
-  const shortestWavelength = rows[0][0];
-  const longestWavelength = rows[rows.length - 1][0];
-  const wavelengthInterval = rows[1][0] - rows[0][0];
-  const interpolatedRows = [];
-  for (
-    let wavelength = shortestWavelength;
-    wavelength <= longestWavelength;
-    wavelength += 1
-  ) {
-    interpolatedRows[wavelength - shortestWavelength] = [wavelength];
-  }
-  for (let locationIdx = 1; locationIdx <= sampleCount; locationIdx += 1) {
-    const locationIrradiances = rows.map((row) => row[locationIdx]);
-    const interpolatedIrradiances = sprague(
-      locationIrradiances,
-      wavelengthInterval
-    );
-    for (
-      let wavelengthIdx = 0;
-      wavelengthIdx < interpolatedRows.length;
-      wavelengthIdx += 1
-    ) {
-      interpolatedRows[wavelengthIdx][locationIdx] =
-        interpolatedIrradiances[wavelengthIdx];
-    }
-  }
-  return interpolatedRows;
 };
 
 export const scaleSamples = (rows, areaUnit, powerUnit) => {
