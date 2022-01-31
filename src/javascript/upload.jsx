5e5f25c035f018d25dbaa7c2a19baab857ecd734
Manuel Spitschan <manuel.spitschan@tum.de>
2022-01-31 23:39:38 +0100

    Update upload.jsx

--- src/javascript/upload.jsx
+++ src/javascript/upload.jsx
@@ -1,1 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "../stylesheets/upload.css";
+import "../stylesheets/results.css";
+import App from "./components/App";
 
+ReactDOM.render(<App />, document.getElementById("app"));
