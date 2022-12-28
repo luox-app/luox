ccbf59d1361721edc48b36b7d9b01401eed542c6
Manuel Spitschan <manuel.spitschan@tum.de>
2022-12-28 16:25:43 +0100

    Update NavBar.jsx

--- src/javascript/components/NavBar.jsx
+++ src/javascript/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = ({ location }) => {
   const isAbout = location.pathname === "/about";
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <img src={luoxIcon} width="10%" alt="luox icon" />
+      <img src={luoxIcon} width="1%" alt="luox icon" />
       <a className="navbar-brand" href="/">
         luox {isHome && <span className="sr-only">(current)</span>}
       </a>
