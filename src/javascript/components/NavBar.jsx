38fb1df125cd0dd4393990616c008204189e8474
Manuel Spitschan <manuel.spitschan@tum.de>
2022-12-28 16:51:14 +0100

    Update NavBar.jsx

--- src/javascript/components/NavBar.jsx
+++ src/javascript/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactRouterPropTypes from "react-router-prop-types";
-import luoxIcon from "../../images/logo.svg";
+import luoxIcon from "../../images/icon.svg";
 
 const NavBar = ({ location }) => {
   const isHome = location.pathname === "/";
@@ -9,7 +9,7 @@ const NavBar = ({ location }) => {
   const isAbout = location.pathname === "/about";
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <img src={luoxIcon} width="5%" alt="luox logo" />{" "}
+      <img src={luoxIcon} width="5%" alt="luox icon" />{" "}
       <a className="navbar-brand" href="/">
         Home {isHome && <span className="sr-only">(current)</span>}
       </a>
